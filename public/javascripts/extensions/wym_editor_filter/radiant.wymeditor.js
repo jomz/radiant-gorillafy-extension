bd237286f5c33b6b7c0c458e6de31931996790eb
Benny Degezelle <benny@gorilla-webdesign.be>
2009-02-24 14:51:02 +0100

    fixing part name regexp

--- public/javascripts/extensions/wym_editor_filter/radiant.wymeditor.js
+++ public/javascripts/extensions/wym_editor_filter/radiant.wymeditor.js
@@ -364,8 +364,7 @@ function unboot_all_wym() {
 	{
 		var parts = $$('.part');
     for(var i=0;i< parts.length;i++) {
-			if (/part-([\w\d-]+)/i.test(parts[i].id))
-	      var part_name = RegExp.$1;
+			var part_name = RegExp.$1;
      	// Find all parts that have WYM set as filter
 			if ($F('part_' + part_name + '_filter_id') == 'WymEditor')
 				unboot_wym($('part_'+ part_name +'_content'));

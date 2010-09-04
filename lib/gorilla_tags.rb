edbccc807e036ea05a0a03714ee18141aaef8bf9
Benny Degezelle <benny@gorilla-webdesign.be>
2010-09-04 21:40:46 +0200

    giving these tags a home

---
+++ lib/gorilla_tags.rb
@@ -0,0 +1,27 @@
+module BlogTags
+  include Radiant::Taggable
+  include ActionView::Helpers::TagHelper # to pass html attributes through tags
+  require "digest" # for r:gravatar
+
+  class TagError < StandardError; end
+  
+  tag "time_ago_in_words" do |tag|
+    ActionView::Base.new.time_ago_in_words(tag.locals.page.published_at || tag.locals.page.created_at)
+  end
+
+  tag "gravatar" do |tag|
+    email = tag.expand
+    raise TagError, "Gravatar tag requires an email attribute" if email.blank?
+    # include MD5 gem, should be part of standard ruby install
+    hash = Digest::MD5.hexdigest(email.downcase)
+
+    if tag.attr
+      html_options = tag.attr.stringify_keys
+      tag_options = tag_options(html_options)
+    else
+      tag_options = nil
+    end
+
+    "<img src=\"http://www.gravatar.com/avatar/#{hash}\" #{tag_options}/>"
+  end
+end

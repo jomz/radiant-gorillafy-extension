8d8776b27605a8827df6fbb8f6b74e6413380787
Benny Degezelle <benny@gorilla-webdesign.be>
2010-09-20 21:16:14 +0200

    adding rfc3339_date and fix_links_for_syndication tags from old blog ext.

--- lib/gorilla_tags.rb
+++ lib/gorilla_tags.rb
@@ -8,6 +8,17 @@ module BlogTags
   tag "time_ago_in_words" do |tag|
     ActionView::Base.new.time_ago_in_words(tag.locals.page.published_at || tag.locals.page.created_at)
   end
+  
+  tag 'rfc3339_date' do |tag|
+    page = tag.locals.page
+    if date = page.published_at || page.created_at
+      date.to_time.xmlschema
+    end
+  end
+  
+  tag 'fix_links_for_syndication' do |tag|
+    fix_for_syndication(tag.expand, "#{tag.globals.page.request.protocol}#{tag.globals.page.request.host_with_port}")
+  end
 
   tag "gravatar" do |tag|
     email = tag.expand
@@ -23,5 +34,9 @@ module BlogTags
     end
 
     "<img src=\"http://www.gravatar.com/avatar/#{hash}\" #{tag_options}/>"
+  end
+  
+  def fix_for_syndication(text, host)
+    text.gsub(/href=('|")([^(http:|mailto:)].*?)('|")/, 'href="' + host + '\2"').gsub(/src=('|")([^http:].*?)('|")/, 'src="' + host + '\2"')
   end
 end
